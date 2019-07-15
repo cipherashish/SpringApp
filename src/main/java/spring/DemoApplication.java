c56a2baff4f23cd5193cee289b3952ffbb577583
cipherashish <ashish.090singh@gmail.com>
2019-07-15 10:13:45 +0530

    update

--- src/main/java/spring/DemoApplication.java
+++ src/main/java/spring/DemoApplication.java
@@ -2,6 +2,7 @@ package spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
@@ -10,5 +11,9 @@ public class DemoApplication extends ServletInitializer{
 	public static void main(String[] args) {
 		ApplicationContext ctx =SpringApplication.run(DemoApplication.class, args);
 	}
+		@Override
+		  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		      return builder.sources(DemoApplication.class);
+		  }
+	}
 
-}
